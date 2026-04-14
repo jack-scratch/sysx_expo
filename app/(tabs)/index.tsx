00b94b5eb1582ed51596b84abfe22917e58d0984
Jack Alma <jackhasakeyboard@gmail.com>
2026-04-14 09:16:55 -0600

    Remove unused "style"

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,26 +10,7 @@ import { Link } from 'expo-router';
 export default function HomeScreen() {
   return (
 		<ThemedView style={styles.stepContainer}>
-			<ThemedText type="subtitle">sysx</ThemedText>
+			<ThemedText>sysx</ThemedText>
 		</ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
