f33b507b7b7d8eb48bbdb4824e3b4a06ba4e08a2
Jack Alma <jackhasakeyboard@gmail.com>
2026-04-14 09:17:20 -0600

    Add ^fmt"plural["linebreak"]"

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,24 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import {
+	Image
+} from 'expo-image';
+import {
+	Platform,
+	StyleSheet
+} from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
+import {
+	HelloWave
+} from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import {
+	ThemedText
+} from '@/components/themed-text';
+import {
+	ThemedView
+} from '@/components/themed-view';
+import {
+	Link
+} from 'expo-router';
 
 export default function HomeScreen() {
   return (
