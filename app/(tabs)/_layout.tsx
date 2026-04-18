d18b4016651b27ee6ff0a529d2a025251c44aae5
Jack Alma <jackhasakeyboard@gmail.com>
2026-04-18 15:03:41 -0600

    Remove ^fmt"plural["trailing "comma""]"

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -14,13 +14,13 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
+        tabBarButton: HapticTab
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />
         }}
       />
     </Tabs>
