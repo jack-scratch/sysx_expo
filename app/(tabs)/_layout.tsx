009b786d077424e42483dcc2dd5aa5af3c88838f
Jack Alma <jackhasakeyboard@gmail.com>
2026-04-18 15:04:07 -0600

    Add ^fmt"plural["linebreak"]"

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,20 @@
-import { Tabs } from 'expo-router';
+import {
+	Tabs
+} from 'expo-router';
 import React from 'react';
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import {
+	HapticTab
+} from '@/components/haptic-tab';
+import {
+	IconSymbol
+} from '@/components/ui/icon-symbol';
+import {
+	Colors
+} from '@/constants/theme';
+import {
+	useColorScheme
+} from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
