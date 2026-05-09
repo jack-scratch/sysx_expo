11b0652cb37581dcf9ba96a162c6afe30598ebf6
Jack Alma <jackhasakeyboard@gmail.com>
2026-05-09 14:04:28 -0600

    Add ^fmt"plural["linebreak"]"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import {
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider
+} from '@react-navigation/native';
+import {
+	Stack
+} from 'expo-router';
+import {
+	StatusBar
+} from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import {
+	useColorScheme
+} from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -15,8 +25,13 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="(tabs)" options={{
+					headerShown: false
+        }} />
+        <Stack.Screen name="modal" options={{
+					presentation: 'modal',
+					title: 'Modal'
+        }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
